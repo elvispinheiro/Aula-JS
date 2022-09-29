02100c6d19fb104bccb9362dc71759413eed9ba6
elvispinheiro <elvis.pinheiro1996@gmail.com>
2022-09-28 21:26:34 -0300

    teste alteração

---
+++ teste.py
@@ -0,0 +1,17 @@
+from audioop import mul
+
+
+print("Hello Word!")
+
+def multiplica(a,b):
+    resultado = a*b
+    return resultado
+
+
+a=100
+b=10
+
+calc=multiplica(a,b)
+multiplica
+
+print(calc)
